facafe8dec1f13d2e221f2abf88ce4dbd7676780
atshdubs <atshdubs@gmail.com>
2021-02-18 13:40:51 +0545

    updated post evaluation

--- routes/admin/evaluation.js
+++ routes/admin/evaluation.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {Evaluation, Rejections} = require("../../models/admin/Evaluation");
+const { Evaluation, Rejections } = require("../../models/admin/Evaluation");
 const evalFunctions = require("./functions/evaluations");
 
 const router = express.Router();
@@ -7,97 +7,92 @@ const router = express.Router();
 // Get All Evaluation
 const AuthController = require("../../controllers/authController");
 
-router.get('/',
- AuthController.authAdmin,
- async(req, res) => {
-   console.log(Evaluation)
-   const getAllEvaluation = await Evaluation.find().populate('seller')
- 
-   res.status(200).json(getAllEvaluation)
-})
+router.get("/", AuthController.authAdmin, async (req, res) => {
+  console.log(Evaluation);
+  const getAllEvaluation = await Evaluation.find().populate("seller");
 
-router.get('/maintenance',AuthController.authAdmin, async(req, res) => {
-    const getAllMaintenance = await Evaluation.find({'maintenance.status' : true})
-    res.status(200).json(getAllMaintenance)
- })
+  res.status(200).json(getAllEvaluation);
+});
 
- router.get('/dryclean',AuthController.authAdmin, async(req, res) => {
-    const getAllDryClean = await Evaluation.find({'dry_cleaning.status' : true})
-    res.status(200).json(getAllDryClean)
- })
+router.get("/maintenance", AuthController.authAdmin, async (req, res) => {
+  const getAllMaintenance = await Evaluation.find({
+    "maintenance.status": true,
+  });
+  res.status(200).json(getAllMaintenance);
+});
 
+router.get("/dryclean", AuthController.authAdmin, async (req, res) => {
+  const getAllDryClean = await Evaluation.find({ "dry_cleaning.status": true });
+  res.status(200).json(getAllDryClean);
+});
 
-router.post('/', 
-AuthController.authAdmin, 
+router.post(
+  "/",
+  AuthController.authAdmin,
 
-async(req, res) => {
+  async (req, res) => {
+    console.log("inside post evaluation");
+    const { seller, items } = req.body;
 
-  console.log("inside post evaluation")
-    const {
-        seller,
-        items
-    } = req.body
+    console.log(req.body);
 
-    console.log(req.body)
-
-
-
-    if(!seller || !items){
-      res.status(404).json({success: false, message: "seller or item is empty"})
+    if (!seller || !items) {
+      res
+        .status(404)
+        .json({ success: false, message: "seller or item is empty" });
     }
 
     // if(seller){
-       
+
     //     evaluationDestructure.seller = req.body.seller
     // }
-    
-try{ 
-    items.forEach(async (post) => {
-      const evaluationDestructure = {}
-      evaluationDestructure.seller = req.body.seller
 
-      if(post.category) {
-       console.log("category")
-        evaluationDestructure.category = post.category
-    }
-    if(post.selling_price) {
-        evaluationDestructure.selling_price = post.selling_price
-    }
-   
-    if(post.color){
-        console.log("color")
-        evaluationDestructure.color = post.color
-    }
- 
-    if(post.detail){
-        evaluationDestructure.detail = post.detail
-    }
-    if(post.brand){
-        console.log("brand")
-        evaluationDestructure.brand = post.brand
-    }
-    if(post.date_of_receipt){
-        evaluationDestructure.date_of_receipt = post.date_of_receipt
-    }
-    if(post.date_of_pickup){
-        evaluationDestructure.date_of_pickup = post.date_of_pickup
-    }
+    try {
+      let arr = [];
 
+      items.forEach(async (post) => {
+        const evaluationDestructure = {};
+        evaluationDestructure.seller = req.body.seller;
 
+        if (post.category) {
+          console.log("category");
+          evaluationDestructure.category = post.category;
+        }
+        if (post.selling_price) {
+          evaluationDestructure.selling_price = post.selling_price;
+        }
 
-    const postEvaluation = new Evaluation(evaluationDestructure)
-    await postEvaluation.save()
-    })
-   
+        if (post.color) {
+          console.log("color");
+          evaluationDestructure.color = post.color;
+        }
 
- 
-        res.status(200).json({success: true, complete: true})
+        if (post.detail) {
+          evaluationDestructure.detail = post.detail;
+        }
+        if (post.brand) {
+          console.log("brand");
+          evaluationDestructure.brand = post.brand;
+        }
+        if (post.date_of_receipt) {
+          evaluationDestructure.date_of_receipt = post.date_of_receipt;
+        }
+        if (post.date_of_pickup) {
+          evaluationDestructure.date_of_pickup = post.date_of_pickup;
+        }
 
+        const postEvaluation = new Evaluation(evaluationDestructure);
+        await postEvaluation.save();
+        arr.push(postEvaluation);
+      });
+
+      console.log(arr);
+      res.status(200).json({ success: true,  evaluations: [...arr] });
+    } catch (err) {
+      res.status(404).json({ message: err });
     }
-    catch( err) {
-        res.status(404).json({message: err})
-    }
-})
+  }
+);
 
 //Get Evaluation By Id
 router.get("/:evaluationId", async (req, res) => {
@@ -119,7 +114,7 @@ router.patch(
       const data = await Evaluation.findOneAndUpdate(
         { _id: req.params.evaluationId },
         {
-          "maintenance.receivedDate": Date.now()
+          "maintenance.receivedDate": Date.now(),
         }
       );
       res.status(200).json({ success: true });
@@ -137,7 +132,7 @@ router.patch(
       const data = await Evaluation.findOneAndUpdate(
         { _id: req.params.evaluationId },
         {
-          "dry_cleaning.receivedDate": Date.now()
+          "dry_cleaning.receivedDate": Date.now(),
         }
       );
       res.status(200).json({ success: true });
@@ -155,7 +150,7 @@ router.patch("/:evaluationId", AuthController.authAdmin, async (req, res) => {
     purchase_price,
     status,
     dry_cleaning,
-    maintenance
+    maintenance,
   } = req.body;
 
   var data = await Evaluation.findById(req.params.evaluationId);
@@ -229,22 +224,23 @@ router.delete("/:evaluationId", AuthController.authAdmin, async (req, res) => {
   }
 });
 
-router.patch("/reject/:evaluationId", AuthController.authAdmin, async (req, res) => {
-  const rejectId = req.params.evaluationId;
-  try {
-    await Evaluation.findOne(
-      { _id: rejectId},
-      function(err, result) {
+router.patch(
+  "/reject/:evaluationId",
+  AuthController.authAdmin,
+  async (req, res) => {
+    const rejectId = req.params.evaluationId;
+    try {
+      await Evaluation.findOne({ _id: rejectId }, function (err, result) {
         let swap = new Rejections(result.toJSON()); //or result.toObject
 
         result.remove();
         swap.save();
         res.status(200).json(swap);
-      }
-    );
-  } catch(err){
-    return res.status(400).json({message: err.message});
+      });
+    } catch (err) {
+      return res.status(400).json({ message: err.message });
+    }
   }
-})
+);
 
 module.exports = router;
